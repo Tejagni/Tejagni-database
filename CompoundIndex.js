629f170b23c7bbe95ea397fb243083c70fc80a8f
Tejagni <145690822+Tejagni@users.noreply.github.com>
2024-03-11 17:48:41 -0400

    CompoundIndex.js

---
+++ CompoundIndex.js
@@ -0,0 +1,9 @@
+db.student.createIndex({student_id: 777777, student_id: 223344},
+{
+			"createdCollectionAutomatically": false,
+			"numIdexesBefore":1,
+			"numIndexesAfter":2,
+			"ok":1
+})
+
+db.student.getIndexes()
